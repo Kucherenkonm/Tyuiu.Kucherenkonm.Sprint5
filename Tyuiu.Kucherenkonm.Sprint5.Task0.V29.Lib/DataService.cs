3218084dc430f2348a62ec3362b4747137db5e61
Надежда Кучеренко <Tyuiu.kucherenkonm@outlook.com>
2025-11-21 20:36:54 +0500

    Task0

--- Tyuiu.Kucherenkonm.Sprint5.Task0.V29.Lib/DataService.cs
+++ Tyuiu.Kucherenkonm.Sprint5.Task0.V29.Lib/DataService.cs
@@ -6,7 +6,7 @@ namespace Tyuiu.Kucherenkonm.Sprint5.Task0.V29.Lib
     {
         public string SaveToFileTextData(int x)
         {
-            string path = $@"{Directory.GetCurrentDirectory()}\OutPutFileTask0.txt";
+            string path = Path.Combine(new string[] { Path.GetTempPath(), "OutputFileTask0.txt" });
             double y = 6.1 * Math.Pow(x,3) + 0.23 * Math.Pow(x,2) + 1.04 * x;
             y = Math.Round(y,3);
             File.WriteAllText(path,Convert.ToString(y));
--- Tyuiu.Kucherenkonm.Sprint5.Task0.V29.Test/DataServiceTest.cs
+++ Tyuiu.Kucherenkonm.Sprint5.Task0.V29.Test/DataServiceTest.cs
@@ -7,7 +7,7 @@ namespace Tyuiu.Kucherenkonm.Sprint5.Task0.V29.Test
         [TestMethod]
         public void ValidSaveToFileTextData()
         {
-            string path = @"C:\Users\stysh\source\repos\Tyuiu.Kucherenkonm.Sprint5\Tyuiu.Kucherenkonm.Sprint5.Task0.V29\bin\Debug\net8.0\OutPutFileTask0.txt";
+            string path = @"C:\Users\stysh\AppData\Local\Temp\OutputFileTask0.txt";
             FileInfo fileInfo = new FileInfo(path);
             bool fileExists = fileInfo.Exists;
             bool wait = true;
