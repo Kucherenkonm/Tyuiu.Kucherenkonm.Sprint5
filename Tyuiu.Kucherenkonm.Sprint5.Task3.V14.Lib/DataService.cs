6e965daca54a0e92c9bb4da0da2d70882a9cb1d5
Надежда Кучеренко <Tyuiu.kucherenkonm@outlook.com>
2025-11-21 23:39:09 +0500

    Task3

---
+++ Tyuiu.Kucherenkonm.Sprint5.Task3.V14.Lib/DataService.cs
@@ -0,0 +1,18 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.Kucherenkonm.Sprint5.Task3.V14.Lib
+{
+    public class DataService : ISprint5Task3V14
+    {
+        public string SaveToFileTextData(int x)
+        {
+            string path = Path.Combine(new string[] { Path.GetTempPath(), "OutputFileTask3.bin" });
+            double y = 4 * Math.Pow(x, 3) / (Math.Pow(x, 3) - 1);
+            y = Math.Round(y, 3);
+            using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.OpenOrCreate)))
+            {
+                writer.Write(BitConverter.GetBytes(y));
+            }
+            return path;
+        }
+    }
+}
---
+++ Tyuiu.Kucherenkonm.Sprint5.Task3.V14/Program.cs
@@ -0,0 +1,33 @@
+﻿using Tyuiu.Kucherenkonm.Sprint5.Task3.V14.Lib;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Спринт #5 | Выполнила: Кучеренко Н. М. | ИИПб - 25 - 1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #5                                                               *");
+        Console.WriteLine("* Тема: Потоковый метод записи данных в бинарный файл                     *");
+        Console.WriteLine("* Задание #3                                                              *");
+        Console.WriteLine("* Вариант #14                                                             *");
+        Console.WriteLine("* Выполнила: Кучеренко Надежда Михайловна | ИИПб - 25 - 1                 *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("*            Вычислить значение выражения и сохранить результат           *");
+        Console.WriteLine("*                             в бинарный файл                             *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        int x = 3;
+        Console.WriteLine("x = " + x);
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        string res = ds.SaveToFileTextData(x);
+        Console.WriteLine("Файл: " + res);
+        Console.WriteLine("Создан!");
+        Console.ReadKey();
+    }
+}
