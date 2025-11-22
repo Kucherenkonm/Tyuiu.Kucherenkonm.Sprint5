e88b462f2d6d9762f1dd91bdf035fcabe6d6699a
Надежда Кучеренко <Tyuiu.kucherenkonm@outlook.com>
2025-11-22 12:08:04 +0500

    Task6

---
+++ Tyuiu.Kucherenkonm.Sprint5.Task6.V26.Test/DataServiceTest.cs
@@ -0,0 +1,26 @@
+﻿using Tyuiu.Kucherenkonm.Sprint5.Task6.V26.Lib;
+namespace Tyuiu.Kucherenkonm.Sprint5.Task6.V26.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckExistsFile()
+        {
+            string path = @"C:\DataSprint5\InPutDataFileTask6V26.txt";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+        [TestMethod]
+        public void ValidCalc()
+        {
+            DataService ds = new DataService();
+            string path = @"C:\DataSprint5\InPutDataFileTask6V26.txt";
+            var res = ds.LoadFromDataFile(path);
+            int wait = 6;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
