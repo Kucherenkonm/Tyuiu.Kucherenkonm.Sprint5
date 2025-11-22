81ac7e489f199900afc416aa4e229db5270ea511
Надежда Кучеренко <Tyuiu.kucherenkonm@outlook.com>
2025-11-22 10:33:08 +0500

    Task4

---
+++ Tyuiu.Kucherenkonm.Sprint5.Task4.V24.Lib/DataService.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.Kucherenkonm.Sprint5.Task4.V24.Lib
+{
+    public class DataService : ISprint5Task4V24
+    {
+        public double LoadFromDataFile(string path)
+        {
+            NumberFormatInfo info = new NumberFormatInfo()
+            {
+                NumberDecimalSeparator = "."
+            };
+            string strX = File.ReadAllText(path);
+            double x = double.Parse(strX,info);
+            double res = Math.Round((Math.Pow(x, -2) + 2) * Math.Sin(x), 3);
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.Kucherenkonm.Sprint5.Task4.V24/Program.cs
@@ -0,0 +1,32 @@
+﻿using Tyuiu.Kucherenkonm.Sprint5.Task4.V24.Lib;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Спринт #5 | Выполнила: Кучеренко Н. М. | ИИПб - 25 - 1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #5                                                               *");
+        Console.WriteLine("* Тема: Чтение данных из текстового файла                                 *");
+        Console.WriteLine("* Задание #4                                                              *");
+        Console.WriteLine("* Вариант #24                                                             *");
+        Console.WriteLine("* Выполнила: Кучеренко Надежда Михайловна | ИИПб - 25 - 1                 *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("*     Прочитать значение из файла и подставить вместо Х в формуле         *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        string path = @"C:\DataSprint5\InPutDataFileTask4V24.txt";
+        Console.WriteLine("Данные находятся в файле: " + path);
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        double res = ds.LoadFromDataFile(path);
+        Console.WriteLine(res);
+        Console.ReadKey();
+    }
+}
